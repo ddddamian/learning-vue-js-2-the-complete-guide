761f130721093e2959a82c6d35c09f9f7da55a2f
Pete <peterwalsh86@hotmail.com>
2018-01-13 20:56:39 +0000

    Update app.js

--- project1/app.js
+++ project1/app.js
@@ -3,63 +3,79 @@ new Vue({
     data: {
         showControls: true,
         showLog: false,
-        player: {
-            health: 100,
-            damageTaken: []
-        },
-        monster: {
-            health: 100,
-            damageTaken: []
-        }
+        playerHealth: 100,
+        monsterHealth: 100,
+        turns: []
     },
     methods: {
         start: function() {
             this.showControls = !this.showControls;
             this.showLog = false;
 
-            this.player.health = 100;
-            this.player.damageTaken = [];
-
-            this.monster.health = 100;
-            this.monster.damageTaken = [];
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.turns = [];
         },
 
         playerHit: function(damage) {
-            playerDamage = Math.floor(Math.random() * damage);
-            this.player.health -= playerDamage;
-            this.player.damageTaken.push("MONSTER HITS PLAYER FOR " + playerDamage);
+            var playerDamage = Math.floor(Math.random() * damage);
+            this.playerHealth -= playerDamage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: "MONSTER HITS PLAYER FOR " + playerDamage
+            })
         },
 
         monsterHit: function(damage) {
-            monsterDamage = Math.floor(Math.random() * damage);
-            this.monster.health -= monsterDamage;
-            this.monster.damageTaken.push("PLAYER HITS MONSTER FOR " + monsterDamage);
+            var monsterDamage = Math.floor(Math.random() * damage);
+            this.monsterHealth -= monsterDamage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: "PLAYER HITS MONSTER FOR " + monsterDamage
+            })
         },
 
         playerHeal: function() {
-            playerHealth = Math.floor(Math.random() * 10);
-            this.player.health += playerHealth;
-            this.player.damageTaken.push("PLAYER HEALS HIMSELF FOR " + playerHealth);
+            var playerHealthBoost = Math.floor(Math.random() * 10);
+            this.playerHealth += playerHealthBoost;
+            this.turns.unshift({
+                isPlayer: true,
+                text: "PLAYER HEALS HIMSELF FOR " + playerHealthBoost
+            })
         },
 
         attack: function() {
             this.showLog = true;
-            this.playerHit(10);
+
             this.monsterHit(10);
+            if(this.monsterHealth <= 0){
+                alert("You won!");
+                this.showControls = true;
+                start();
+            }
+
+            this.playerHit(10);
+            if(this.playerHealth <= 0){
+                alert("You lost!");
+                start();
+            }
         },
 
         specialAttack: function() {
             this.showLog = true;
-            this.playerHit(20);
             this.monsterHit(20);
+            this.playerHit(20);
         },
 
         heal: function() {
             this.showLog = true;
+            if(this.playerHealth <= 90) {
+                this.playerHeal(); 
+            }
+
             this.playerHit(10);
-            this.playerHeal();  
         },
-        
+
         quit: function() {
             this.showControls = true;
         }
